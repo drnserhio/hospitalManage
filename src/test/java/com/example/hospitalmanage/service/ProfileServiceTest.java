3a432a1a2078175b9c6a6ee0c72b58587d260a6b
drnserhio <duran199756@gmail.com>
2022-02-25 22:10:29 +0200

    /

--- src/test/java/com/example/hospitalmanage/service/ProfileServiceTest.java
+++ src/test/java/com/example/hospitalmanage/service/ProfileServiceTest.java
@@ -1,15 +1,20 @@
 package com.example.hospitalmanage.service;
 
+import com.example.hospitalmanage.dto.RequestTabel;
+import com.example.hospitalmanage.exception.domain.PasswordNotValidException;
+import com.example.hospitalmanage.exception.domain.UserNotFoundException;
+import com.example.hospitalmanage.model.Treatment;
+import org.junit.Test;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
 
 @RunWith(MockitoJUnitRunner.class)
 @ExtendWith(MockitoExtension.class)
@@ -17,4 +22,54 @@ import org.springframework.test.context.junit4.SpringRunner;
 @TestPropertySource(locations = "classpath:application-test.yml")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ProfileServiceTest {
+
+    @Test
+    public void shouldReturnGenerateDocument(String username) {
+
+    }
+
+    @Test
+    public void updateUserTimeVisitByUsername(String currentUsername, LocalDateTime timeVisit) {
+
+    }
+
+    @Test
+    public void addDiagnosis(Long userId, String icdName) {
+
+    }
+
+    @Test
+    public void addTreatment(Long userId, String treatment) {
+
+    }
+
+    @Test
+    public void deleteChooseTreatment(Long userId, Long treatmentId) {
+
+    }
+
+    @Test
+    public void changePassByUsernameAndOldPassword(String oldPassword, String newPassword) throws UserNotFoundException, PasswordNotValidException {
+
+    }
+
+    @Test
+    public void changeHospitalisation(String username, String hospitalization) {
+
+    }
+
+    @Test
+    public void findAllDiagnosisByUser(RequestTabel request, Long id) {
+
+    }
+
+    @Test
+    public void deleteAnalize(Long userId, Long analizeId) {
+
+    }
+
+    @Test
+    public void updateTreatment(Treatment treatment) {
+
+    }
 }
